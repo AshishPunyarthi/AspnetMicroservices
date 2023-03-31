c0c557aefe1ccfcf5991a61d3156c009340d6179
AshishPunyarthi <ashpunyarthi@gmail.com>
2023-03-31 12:33:43 +0530

    Basket.API service with docker-compose

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IBasketRepository basketRepository, ILogger<BasketController> logger)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasketByUsername")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBasket(string userName)
+        {
+            ShoppingCart basket = await _basketRepository.GetBasketAsync(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasketAsync(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasketByUsername")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasketAsync(userName);
+            return Ok();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,31 @@
+using Basket.API.Repositories;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
